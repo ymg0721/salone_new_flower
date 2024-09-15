e45114302bfb3234e507228cc4be94482df6cb53
anyappyoshinagamahiro <162930606+anyappyoshinagamahiro@users.noreply.github.com>
2024-09-15 15:30:39 +0900

    nuxt初期化

---
+++ src/pages/top.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>トップページ</h1>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+    // ここにTypeScriptのコードを記述
+  </script>
+  
+  <style scoped>
+  /* スタイルを記述 */
+  </style>
